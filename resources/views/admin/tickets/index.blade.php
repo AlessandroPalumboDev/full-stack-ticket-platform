960193965077d98ff2ec45c4db4cbe0984e373a8
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-11-24 13:22:22 +0100

    FIX correzione paginazione

--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -70,8 +70,27 @@
         </tbody>
     </table>
 
-    {{ $tickets->links() }}
+    {{-- {{ $tickets->links() }} --}}
 
+
+    {{ $tickets->links('pagination::bootstrap-5') }}
+
+
+    {{-- <ul class="pagination">
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    </ul> --}}
+    
 </div>
 
 {{-- Modale Assegna/Cambia Operatore --}}
